8d3d2106a2e79f84b13394466d64590018f5f4b8
vinhnguyen500 <33370176+vinhnguyen500@users.noreply.github.com>
2018-05-24 22:15:29 -0700

    Add files via upload

---
+++ BluetoothHub/AddWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static BluetoothHub.MainWindow;
+
+namespace BluetoothHub
+{
+    /// <summary>
+    /// Interaction logic for AddWindow.xaml
+    /// </summary>
+    public partial class AddWindow : Window
+    {
+        BluetoothDeviceInfo[] devicesList;
+        ObservableCollection<DeviceItem> foundItems = new ObservableCollection<DeviceItem>();
+        ObservableCollection<DeviceItem> AddedDevices;
+
+        public AddWindow(ObservableCollection<DeviceItem> collection)
+        {
+            InitializeComponent();
+            AddedDevices = collection;
+            Scan();
+            lbFoundDevices.ItemsSource = foundItems;
+        }
+
+        private void DeviceListButton_Click(object sender, RoutedEventArgs e)
+        {
+            DeviceItem item = (DeviceItem)(sender as Button).DataContext;
+            foundItems.Remove(item);
+            AddedDevices.Add(item);
+        }
+
+        private void Scan()
+        {
+            ScanStatus.Text = "Scanning for devices...";           
+            //Create a new instance of the BluetoothClient class
+            BluetoothClient thisDevice = new BluetoothClient();
+            //Create a new instance of BluetoothComponent by passing this device
+            BluetoothComponent component= new BluetoothComponent(thisDevice);
+
+            //Add an event listener for when a device is discovered
+            component.DiscoverDevicesProgress += BluetoothComponent_DiscoverDevicesProgress;
+            //Add an event listener for when the discovery process is finished
+            component.DiscoverDevicesComplete += BluetoothComponent_DiscoverDevicesComplete;
+
+            //Start discovery of devices
+            component.DiscoverDevicesAsync(8, true, true, true, false, thisDevice);
+        }
+
+        //Called everytime a new device is discovered
+        void BluetoothComponent_DiscoverDevicesProgress(object sender, DiscoverDevicesEventArgs e)
+        {
+            //Get the device(s) that was just discovered
+            devicesList = e.Devices;
+        }
+
+        Object Source(String type)
+        {
+            if (type.Contains("Audio"))
+            {
+                return Application.Current.FindResource("HeadphonesImage");
+            }
+            if (type.Contains("Computer"))
+            {
+                return Application.Current.FindResource("ComputerImage");
+            }
+            if (type.Contains("Keyboard"))
+            {
+                return Application.Current.FindResource("KeyboardImage");
+            }
+            if (type.Contains("Peripheral"))
+            {
+                return Application.Current.FindResource("MouseImage");
+            }
+            return Application.Current.FindResource("DeviceImage");
+        }
+
+        //Called after the discovery process is completed
+        void BluetoothComponent_DiscoverDevicesComplete(object sender, DiscoverDevicesEventArgs e)
+        {
+            ScanStatus.BeginAnimation(OpacityProperty, null);
+            if (devicesList == null)
+            {
+                ScanStatus.Text = "No devices found";
+            }
+            else
+            {
+                ScanStatus.Text = "Select a device below to connect";
+                foreach (BluetoothDeviceInfo device in devicesList)
+                {
+                    foundItems.Add(new DeviceItem() { Title = device.DeviceName, Level = 100, Source = Source(device.ClassOfDevice.MajorDevice.ToString()) });
+                }
+            }
+        }
+    }
+}
---
+++ BluetoothHub/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using InTheHand.Net.Sockets;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BluetoothHub
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<DeviceItem> items = new ObservableCollection<DeviceItem>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DevicesIC.ItemsSource = items;
+        }
+
+        public class DeviceItem
+        {
+            public string Title { get; set; }
+            public int Level { get; set; }
+            public Object Source { get; set; }
+        }
+
+        private void AddDevice_Click(object sender, RoutedEventArgs e)
+        {
+            AddWindow addWindow = new AddWindow(items)
+            {
+                Owner = Application.Current.MainWindow
+            };
+            addWindow.ShowDialog();
+        }
+
+        private void Remove_Click(object sender, RoutedEventArgs e)
+        {
+            DeviceItem item = (DeviceItem)(sender as Button).DataContext;
+            items.Remove(item);
+        }
+
+
+    }
+}
